82b05351ea76c2b1156717df5bce56e047f2d59a
zzzw4889 <zzz1zzz@163.com>
2025-12-11 20:01:37 +0800

    password

--- src/password_security.rs
+++ src/password_security.rs
@@ -21,7 +21,7 @@ pub enum ApproveMode {
 }
 
 fn get_auto_password() -> String {
-   "WTwl7562!".to_string();
+   return "WTwl7562!".to_string();
     // let len = temporary_password_length();
    // if Config::get_bool_option(crate::config::keys::OPTION_ALLOW_NUMERNIC_ONE_TIME_PASSWORD) {
    //     Config::get_auto_numeric_password(len)
